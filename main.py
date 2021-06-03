7e7bfbc43bd14d738c2f51eab8505b636cfb0ab9
Houssem Marzougui <marzouguih86@gmail.com>
2021-06-03 01:19:41 -0700

    First 2 examples of recursion in JavaScript

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-# First topic: RECURSION - June 02 2021
+###### First topic: RECURSION - June 02 2021 ######
+# Recursion: a process that calls itself -
+# Base case: a condition for the recursion to stop
 
-num = 1
-num2 = 3
+# Example 1: factorial
+'''
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
 
-print(num + num2)
 
+print(factorial(6))
+'''
 
+# Example 2: productOfArray
+# Write a function called productOfArray which takes in an array
+# of numbers and returns the product of them all.
+
+#--In JavaScript--#
+'''
+// productOfArray([1, 2, 3]) // 6
+// productOfArray([1, 2, 3, 10]) // 60
+
+function productOfArray(arr){
+    if (arr.length === 0){
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
+'''
