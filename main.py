29e92a909c9c6713c409ffa47ea6aac9bff03475
Houssem Marzougui <marzouguih86@gmail.com>
2021-06-03 11:54:05 -0700

    Example 4: Fibonacci Sequence

--- main.py
+++ main.py
@@ -65,3 +65,32 @@ print(recursiveRange(6))
 print(recursiveRange(10))
 '''
 
+# Example 4: Fibonacci
+# Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence.
+# Recall that the Fibonacci sequence is the sequence of whole numbers 1, 1, 2, 3, 5, 8, ...
+# which starts with 1 and 1, and where every number thereafter is equal to the sum of
+# the previous two numbers. .
+
+#--In JavaScript--#
+'''
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
+
+function fib(num){
+    if (num === 1 | | num == = 2){
+       return 1;
+    }
+   return fib(num - 1) + fib(num - 2);
+}
+'''
+
+#--In Python--#
+
+def fib(num):
+    if num == 1 or num == 2:
+        return 1
+    return fib(num-1)+fib(num-2)
+
+print(fib(7))
