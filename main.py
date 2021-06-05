9b49b889b7d7bb1a50cb306bda53a1aec1d717ac
Houssem Marzougui <marzouguih86@gmail.com>
2021-06-05 15:03:11 -0700

    Example 4: Bubble Sort algorithm -- optimized JS code with noSwaps variable

--- main.py
+++ main.py
@@ -349,14 +349,18 @@ print(KMP_search('myhellonameishelhoussemloandhelloagain','hello'))
 '''
 function BubbleSort(arr){
     let temp;
+    let noSwaps;
     for(let i = arr.length; i >= 0; i--){
+        noSwaps = true;
         for(let j = 0; j < i-1; j++){
             if(arr[j] > arr[j+1]){
                 temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+                noSwaps = true;
             }
         }
+        if(noSwaps) break;
     }
     return arr;
 }
